155d6a26055efc00788eb8d915d7d5135d1e1725
Gaia Rossi <rssgai@unife.it>
2025-12-17 11:13:34 +0100

    update criu_dump.sh to use the tar in the curl response

--- kubernetes/daemonsets/priviledged/criu_dump.sh
+++ kubernetes/daemonsets/priviledged/criu_dump.sh
@@ -5,19 +5,13 @@ set -euo pipefail
 
 URL="https://127.0.0.1:10250/checkpoint/${CRIU_DUMP_NAMESPACE}/${CRIU_DUMP_POD_NAME}/${CRIU_DUMP_CONTAINER_NAME}"
 
-if curl -k -f -X POST \
-    -H "Authorization: Bearer ${CRIU_DUMP_TOKEN}" \
-    "${URL}"; then
-    echo "Dump successful."
-else
-    echo "Dump failed."
-    exit 1
-fi
+RESP="$(curl -fsSk -X POST -H "Authorization: Bearer ${CRIU_DUMP_TOKEN}" "${URL}")"
+
+TAR_PATH="$(echo ${RESP} | jq -r '.items[0] // empty')"
 
 # Image creation with buildah
 newcontainer=$(buildah from scratch)
-buildah add "$newcontainer" \
-    "/var/lib/kubelet/checkpoints/checkpoint-${CRIU_DUMP_POD_NAME}_${CRIU_DUMP_NAMESPACE}-${CRIU_DUMP_CONTAINER_NAME}"-*.tar /
+buildah add "$newcontainer" "${TAR_PATH}" /
 buildah config --annotation="io.kubernetes.cri-o.annotations.checkpoint.name=${CRIU_DUMP_CONTAINER_NAME}" "$newcontainer"
 buildah commit "$newcontainer" checkpoint-image:"${CRIU_DUMP_POD_NAME}_${CRIU_DUMP_CONTAINER_NAME}"
 buildah rm "$newcontainer"
